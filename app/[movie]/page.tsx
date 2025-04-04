5fe1775b0fb8e5b7fffce7fd859d821de2155fe7
Daniel <danieldavydov0@gmail.com>
2025-04-04 17:45:29 -0400

    api working

--- app/[movie]/page.tsx
+++ app/[movie]/page.tsx
@@ -4,6 +4,12 @@ export default async function Movie({
   params: Promise<{ movie: string }>;
 }) {
   const { movie } = await params;
+  const res = await fetch(
+    `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${movie}`
+  );
+  const data = await res.json();
+  console.log(data);
+
   return (
     <div>
       <p>TESTING TESTING {movie} </p>
--- types.ts
+++ types.ts
@@ -1,4 +1,4 @@
-export interface OMDbMovie {
+export interface MovieProps {
     Title?: string;
     Year?: string;
     Rated?: string;
