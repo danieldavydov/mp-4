e4dca6673aa5a9d7b44a912a32fb43f15e6d0674
Daniel <danieldavydov0@gmail.com>
2025-04-04 18:46:56 -0400

    Error handling for API working

--- app/[movie]/page.tsx
+++ app/[movie]/page.tsx
@@ -1,18 +1,22 @@
+import { MovieProps } from "@/types";
+
 export default async function Movie({
   params,
 }: {
   params: Promise<{ movie: string }>;
 }) {
   const { movie } = await params;
+  const API_KEY = process.env.API_KEY;
   const res = await fetch(
-    `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${movie}`
+    `http://www.omdbapi.com/?apikey=${API_KEY}&t=${movie}`
   );
-  const data = await res.json();
+  const data: MovieProps = await res.json();
   console.log(data);
 
   return (
     <div>
-      <p>TESTING TESTING {movie} </p>
+      {data.Response === "True" && <p>Success</p>}
+      {data.Response === "False" && <p>Something went wrong, failed request</p>}
     </div>
   );
 }
