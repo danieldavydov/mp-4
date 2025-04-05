b7588fac067c67d07590805283f68b1cd6370f2a
Daniel <danieldavydov0@gmail.com>
2025-04-04 22:53:59 -0400

    fix deployment issue: unused variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Indie_Flower } from "next/font/google";
+import { Indie_Flower } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const indieFlower = Indie_Flower({
   weight: "400",
